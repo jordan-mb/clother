62e3b2ed1fff53678c408334d53217b74a7b5e90
Jordan Hamel <jordan@nearbuysystems.com>
2013-06-27 14:33:28 -0700

    initial commit

---
+++ development/ios/device-ios.app/assets/forge/app_config.js
@@ -0,0 +1,63 @@
+window.forge = {}; window.forge.config = {
+    "author": "jo.mravca@gmail.com", 
+    "config_hash": "cbb36b43af28d30ed0372b317b936dbd73531394", 
+    "config_version": "4", 
+    "core": {
+        "android": {
+            "package_name": "io.trigger.forgeb3784e18de1e11e2903e12313d00dc45"
+        }, 
+        "firefox": {
+            "package_name": "b3784e18de1e11e2903e12313d00dc45"
+        }, 
+        "general": {
+            "reload": true
+        }, 
+        "ie": {
+            "package_name": "{80C7B560-767C-B5C5-E86D-4665ED001327}"
+        }, 
+        "ios": {
+            "package_name": "io.trigger.forgeb3784e18de1e11e2903e12313d00dc45"
+        }, 
+        "osx": {
+            "package_name": "io.trigger.forgeb3784e18de1e11e2903e12313d00dc45"
+        }, 
+        "safari": {
+            "package_name": "forge.safari.clotherb3784e18de1e11e2903e12313d00dc45"
+        }
+    }, 
+    "description": "An empty app created by default", 
+    "json_safe_name": "Clother", 
+    "logging": {
+        "level": "DEBUG"
+    }, 
+    "name": "Clother", 
+    "package_name": "clotherb3784e18de1e11e2903e12313d00dc45", 
+    "platform_version": "v1.4.49", 
+    "plugins": {
+        "contact": {
+            "hash": "notahash"
+        }, 
+        "file": {
+            "hash": "notahash"
+        }, 
+        "media": {
+            "hash": "notahash"
+        }, 
+        "prefs": {
+            "hash": "notahash"
+        }, 
+        "request": {
+            "config": {
+                "permissions": [
+                    "http://*/*", 
+                    "https://*/*"
+                ]
+            }, 
+            "hash": "notahash"
+        }
+    }, 
+    "trigger_domain": "https://trigger.io", 
+    "uuid": "b3784e18de1e11e2903e12313d00dc45", 
+    "version": "0.1", 
+    "xml_safe_name": "Clother"
+};
